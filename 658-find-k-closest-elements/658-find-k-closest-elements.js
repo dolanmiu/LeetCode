a124f5a37805da034d529885267798726e6e940b
Dolan <dolan_miu@hotmail.com>
2022-04-03 23:39:13 +0100

    Time: 166 ms (24.64%), Space: 51.1 MB (10.17%) - LeetHub

---
+++ 658-find-k-closest-elements/658-find-k-closest-elements.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} x
+ * @return {number[]}
+ */
+var findClosestElements = function(arr, k, x) {
+    const diffArr = arr.map(n => Math.abs(n - x));
+    
+    let currSum = 0;
+    for (let i = 0; i < k; i++) {
+        currSum += diffArr[i];
+    }
+    
+    let minSum = currSum;
+    let minIndexes = [0, k - 1];
+    
+    let j = k;
+    for (let i = 1; j < diffArr.length; i++, j++) {
+        const sum = minSum - diffArr[i - 1] + diffArr[j];
+        
+        if (sum < minSum) {
+            minSum = sum;
+            minIndexes = [i, j];
+        }
+    }
+    
+    const output = [];
+    
+    for (let i = minIndexes[0]; i <= minIndexes[1]; i++) {
+        output.push(arr[i]);
+    }
+    console.log(minIndexes, output)
+
+    return output;
+};
