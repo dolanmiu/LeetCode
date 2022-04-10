13cac0c14fc84127a2002cf7f6c0d0343bd51465
Dolan <dolan_miu@hotmail.com>
2022-04-10 01:35:19 +0100

    Time: 132 ms (71.13%), Space: 59.6 MB (19.07%) - LeetHub

--- 648-replace-words/648-replace-words.js
+++ 648-replace-words/648-replace-words.js
@@ -5,16 +5,88 @@
  */
 // Time: O(n * m)
 // Space: O(n)
+// var replaceWords = function(dictionary, sentence) {
+//     const words = sentence.split(" ");
+    
+//     for (let i = 0; i < words.length; i++) {
+//         for (let j = 0; j < dictionary.length; j++) {
+//             if (words[i].startsWith(dictionary[j])) {
+//                 words[i] = dictionary[j];
+//             }
+//         }
+//     }
+    
+//     return words.join(" ");
+// };
 var replaceWords = function(dictionary, sentence) {
-    const words = sentence.split(" ");
+    const trie = createTrie(dictionary);
+    const dicSet = new Set(dictionary);
+    
+    const words = sentence.split(' ');
     
     for (let i = 0; i < words.length; i++) {
-        for (let j = 0; j < dictionary.length; j++) {
-            if (words[i].startsWith(dictionary[j])) {
-                words[i] = dictionary[j];
-            }
+        const traversedWord = checkWordInTrie(trie, words[i], dicSet);
+        if (dicSet.has(traversedWord)) {
+            words[i] = traversedWord;
         }
     }
     
-    return words.join(" ");
-};
+    return words.join(' ');
+}
+
+function checkWordInTrie(trie, word, dicSet) {
+    let currWord = '';
+    let currNode = trie;
+    const letters = word.split('');
+    
+    while (currNode !== undefined) {
+        const letter = letters.shift();
+        if (currNode.children[letter]) {
+            currWord += letter;
+        }
+        
+        currNode = currNode.children[letter];
+        if (dicSet.has(currWord)) {
+            return currWord;
+        }
+    }
+    
+    return currWord;
+}
+
+function createTrie(dictionary) {
+    const head = new TrieNode('');
+    
+    for (const word of dictionary) {
+        dfs(head, word.split(''));
+    }
+    
+    return head;
+}
+
+function dfs(node, currWord) {
+    if (currWord.length === 0) {
+        return;
+    }
+    
+    const char = currWord.shift();
+    
+    if (!node.children[char]) {
+        const newNode = new TrieNode(char);
+        node.children[char] = newNode;
+    }
+    
+    dfs(node.children[char], currWord);
+
+} 
+
+class TrieNode {
+    constructor(val) {
+        this.val = val;
+        this.children = {};
+    }
+    
+    isLeaf() {
+        return this.children.length === 0;
+    }
+}
