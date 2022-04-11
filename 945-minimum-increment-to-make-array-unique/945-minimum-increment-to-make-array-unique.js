57a8a1d71708ed13c833b21fa7ef6bb46e4484db
Dolan <dolan_miu@hotmail.com>
2022-04-11 23:36:21 +0100

    Time: 109 ms (100.00%), Space: 55.2 MB (41.82%) - LeetHub

---
+++ 945-minimum-increment-to-make-array-unique/945-minimum-increment-to-make-array-unique.js
@@ -0,0 +1,66 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// var minIncrementForUnique = function(nums) {
+//     const mem = new Map();
+    
+//     for (const n of nums) {
+//         mem.set(n, (mem.get(n) ?? 0) + 1);
+//     }
+    
+//     const keys = Array.from(mem.keys());
+    
+//     const minKey = keys.reduce((acc, curr) => Math.min(acc, curr));
+//     const maxKey = keys.reduce((acc, curr) => Math.max(acc, curr));
+    
+//     let pointer = minKey;
+//     let output = 0;
+
+//     for (let k = 0; k < keys.length; k++) {
+//         const i = keys[k];
+//         const count = mem.get(i);
+//         pointer = i + 1;
+        
+//         for (let j = 0; j < count - 1; j++) {
+//             inner:
+//             while (true) {
+//                 if (mem.get(pointer) === undefined) {
+//                     output += pointer - i;
+//                     mem.set(pointer, 1);
+//                     pointer++;
+//                     break inner;
+//                 }
+//                 pointer++;
+//             }
+//         }
+        
+//     }
+
+//     return output;
+// };
+var minIncrementForUnique = function(nums) {
+    const maxVal = nums.reduce((acc, curr) => Math.max(acc, curr));
+    
+    const count = Array(nums.length + maxVal).fill(0);
+    
+    for (const n of nums) {
+        count[n]++;
+    }
+    
+    let moves = 0;
+    let taken = 0;
+    
+    for (let x = 0; x < nums.length + maxVal; x++) {
+        if (count[x] >= 2) {
+            const extra = count[x] - 1;
+            taken += extra;
+            moves -= x * extra;
+        } else if (taken > 0 && count[x] === 0) {
+            taken--;
+            moves += x;
+        }
+    }
+    
+    return moves;
+}
