aea3e615663c5ccaec8dce2e5e9f0aea4b3f467f
Dolan <dolan_miu@hotmail.com>
2022-04-20 22:21:36 +0100

    Time: 93 ms (67.01%), Space: 43.5 MB (21.92%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -7,10 +7,10 @@ var twoSum = function(nums, target) {
     const mem = new Map();
     
     for (let i = 0; i < nums.length; i++) {
-        const diff = nums[i] - target;
+        const diff = target - nums[i];
         
-        if (mem.has(-diff)) {
-            return [i, mem.get(-diff)];
+        if (mem.has(diff)) {
+            return [i, mem.get(diff)];
         }
         
         mem.set(nums[i], i);
