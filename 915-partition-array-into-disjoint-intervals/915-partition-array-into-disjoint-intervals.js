d66beba1662e7e0c61738018cde1a35e8c5ba2ef
Dolan <dolan_miu@hotmail.com>
2022-04-25 20:54:34 +0100

    Time: 107 ms (69.05%), Space: 52.6 MB (76.19%) - LeetHub

--- 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.js
+++ 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.js
@@ -2,6 +2,8 @@
  * @param {number[]} nums
  * @return {number}
  */
+// Time: O(n)
+// Space: O(1)
 var partitionDisjoint = function(nums) {
     let currMax = nums[0];
     let possibleMax = nums[0];
@@ -15,5 +17,7 @@ var partitionDisjoint = function(nums) {
         possibleMax = Math.max(possibleMax, nums[i]);
     }
     
+    // Need to increment 1 because currIndex is the current accepted border
+    // Adding one will step into where it needs to be sliced
     return currIndex + 1;
 };
