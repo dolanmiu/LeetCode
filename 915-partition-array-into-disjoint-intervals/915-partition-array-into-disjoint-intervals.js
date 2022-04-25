19d85a91e3be6a5e25b8cfb6d60d83f7d9fbaa81
Dolan <dolan_miu@hotmail.com>
2022-04-25 19:30:25 +0100

    Time: 127 ms (61.90%), Space: 53.2 MB (61.90%) - LeetHub

---
+++ 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var partitionDisjoint = function(nums) {
+    let currMax = nums[0];
+    let possibleMax = nums[0];
+    let currIndex = 0;
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (currMax > nums[i]) {
+            currIndex = i;
+            currMax = possibleMax;
+        } else {
+            possibleMax = Math.max(possibleMax, nums[i]);
+        }
+    }
+    
+    return currIndex + 1;
+};
