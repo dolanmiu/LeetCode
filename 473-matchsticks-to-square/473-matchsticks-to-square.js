07413b34ce136732d1ef3482d81ccc40f24be701
Dolan <dolan_miu@hotmail.com>
2022-04-30 00:37:45 +0100

    Time: 1713 ms (5.32%), Space: 48.5 MB (12.77%) - LeetHub

---
+++ 473-matchsticks-to-square/473-matchsticks-to-square.js
@@ -0,0 +1,59 @@
+/**
+ * @param {number[]} matchsticks
+ * @return {boolean}
+ */
+// Time: O(n)
+let target;
+var makesquare = function(matchsticks) {
+    const sum = matchsticks.reduce((acc, curr) => acc + curr);
+    
+    if (sum % 4 !== 0) {
+        return false
+    }
+    
+    target = sum / 4;
+    
+    const output = dfs(matchsticks, [0, 0, 0, 0], 0);
+    
+    return output;
+    
+};
+
+function dfs(matchsticks, buckets, index) {   
+    const [left, right, up, down] = buckets;
+    
+    if (left === target && right === target && up === target && down === target) {
+        return true;
+    }
+    
+    if (left > target || right > target || up > target || down > target) {
+        return false;
+    }
+    
+    if (index > matchsticks.length) {
+        return false;
+    }
+    
+    let output = false;
+    
+    outer:
+    for (let i = 0; i < 4; i++) {
+        inner:
+        for (let j = 0; j < 4; j++) {
+            if (j === i) {
+                break inner;
+            }
+            
+            if (buckets[i] === buckets[j]) {
+                continue outer;
+            }
+        }
+        
+        const newBuckets = [...buckets];
+        newBuckets[i] += matchsticks[index];
+        
+        output |= dfs(matchsticks, newBuckets, index + 1);
+    }
+    
+    return output;
+}
