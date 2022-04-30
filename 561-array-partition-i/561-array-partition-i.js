c92a01476c90616e02efbfab48abd129592c2b04
Dolan <dolan_miu@hotmail.com>
2022-04-30 23:04:51 +0100

    Time: 142 ms (63.35%), Space: 51.6 MB (5.47%) - LeetHub

--- 561-array-partition-i/561-array-partition-i.js
+++ 561-array-partition-i/561-array-partition-i.js
@@ -6,6 +6,6 @@ var arrayPairSum = function(nums) {
     return nums.sort((a, b) => a - b)
         .map((n, i) => i % 2 === 0 ? [n, nums[i + 1]] : undefined)
         .filter((a) => !!a)
-        .map(([a, b]) => Math.min(a, b))
+        .map((a) => Math.min(...a))
         .reduce((acc, curr) => acc + curr);
 };
