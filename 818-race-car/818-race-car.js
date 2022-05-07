0c32dd3b6240a3c470600e59a3c54fea5ac8214e
Dolan <dolan_miu@hotmail.com>
2022-05-07 17:59:09 +0100

    Time: 3519 ms (5.05%), Space: 73.5 MB (57.57%) - LeetHub

---
+++ 818-race-car/818-race-car.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number} target
+ * @return {number}
+ */
+let mem;
+var racecar = function(target) {
+    mem = new Set();
+    const output = bfs(target);
+    
+    return output;
+};
+
+function bfs(target) {
+    const queue = [{ pos: 0, speed: 1, length: 0 }];
+    while (queue.length > 0) {
+        const curr = queue.shift();
+        
+        if (curr.pos === target) {
+            return curr.length;
+        }
+        
+        const nextAcceleratePos = curr.pos + curr.speed;
+        if (!mem.has(`${nextAcceleratePos}-${curr.speed * 2}`) && nextAcceleratePos > 0 && nextAcceleratePos < (target << 1)) {
+            queue.push({ pos: nextAcceleratePos, speed: curr.speed * 2, length: curr.length + 1 }); // accelerate
+            mem.add(`${nextAcceleratePos}-${curr.speed * 2}`);
+        }
+        
+        if (!mem.has(`${curr.pos}-${curr.speed > 0 ? -1 : 1}`) && curr.pos > 0 && curr.pos < (target << 1)) {
+            queue.push({ pos: curr.pos, speed: curr.speed > 0 ? -1 : 1, length: curr.length + 1 }); // reverse
+            mem.add(`${curr.pos}-${curr.speed > 0 ? -1 : 1}`);
+        }
+    }
+    
+    return -1;
+}
