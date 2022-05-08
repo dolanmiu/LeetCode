23ea2c3e4eae620cdc6556ca1a8c2bd8a2420736
Dolan <dolan_miu@hotmail.com>
2022-05-08 16:56:39 +0100

    Time: 195 ms (54.76%), Space: 52.1 MB (90.08%) - LeetHub

---
+++ 359-logger-rate-limiter/359-logger-rate-limiter.js
@@ -0,0 +1,29 @@
+
+var Logger = function() {
+    this.debounce = new Map();
+};
+
+/** 
+ * @param {number} timestamp 
+ * @param {string} message
+ * @return {boolean}
+ */
+Logger.prototype.shouldPrintMessage = function(timestamp, message) {
+    if (this.debounce.has(message)) {
+        if (timestamp >= this.debounce.get(message) + 10) {
+            this.debounce.set(message, timestamp);
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        this.debounce.set(message, timestamp);
+        return true;
+    }
+};
+
+/** 
+ * Your Logger object will be instantiated and called as such:
+ * var obj = new Logger()
+ * var param_1 = obj.shouldPrintMessage(timestamp,message)
+ */
