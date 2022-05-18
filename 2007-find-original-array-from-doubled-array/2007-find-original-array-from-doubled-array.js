348db4acc66892ce84d976493d55c1310f5c14d1
Dolan <dolan_miu@hotmail.com>
2022-05-18 20:28:01 +0100

    Time: 4446 ms (9.87%), Space: 122.6 MB (5.55%) - LeetHub

---
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.js
@@ -0,0 +1,111 @@
+/**
+ * @param {number[]} changed
+ * @return {number[]}
+ */
+var findOriginalArray = function(changed) {
+    changed.sort((a, b) => a - b);
+    
+    const count = new Map();
+    
+    for (const n of changed) {
+        count.set(n, (count.get(n) ?? 0) + 1);
+    }
+    
+    if (count.has(0)) {
+        if (count.get(0) % 2 !== 0) {
+            return [];
+        }
+    }
+    console.log(changed)
+    const output = new Map();
+    const used = new Map();
+    
+    for (let i = 0; i < changed.length; i++) {
+        const curr = changed[i];
+        
+        const currCount = count.get(curr);
+
+        if ((used.get(curr) ?? 0) + (output.get(curr) ?? 0) >= currCount) {
+            continue;
+        }
+        
+        const double = curr * 2;
+        console.log(double, count.get(double), used.get(double), count.get(double))
+        
+        if (count.has(double) && (used.get(double) ?? 0) < count.get(double)) {
+            used.set(double, (used.get(double) ?? 0) + 1);
+            output.set(curr, (output.get(curr) ?? 0) + 1);
+            // count.set(double, count.get(double) - 1);
+        }
+        
+        
+    }
+    
+    const res = [];
+    
+    for (const [key, value] of output) {
+        for (let i = 0; i < value; i++) {
+            res.push(key);
+        }
+    }
+    
+    console.log(res, output)
+    
+    if (res.length !== changed.length / 2) {
+        return [];
+    }
+    
+    return res;
+    
+}
+
+// var findOriginalArray = function(changed) {
+//     const count = new Map();
+    
+//     for (const n of changed) {
+//         count.set(n, (count.get(n) ?? 0) + 1);
+//     }
+    
+//     const res = Array(changed.length / 2).fill(0);
+//     let index = 0;
+    
+//     for (const [currKey, value] of count) {
+//         let key = currKey;
+//         if (key === 0) {
+//             const freq = count.get(key);
+//             if (freq % 2 === 0) {
+//                 // If there are even number of 0s, then its impossible, return blank
+//                 return [];
+//             }
+            
+//             for (let i = 0; i < freq / 2; i++) {
+//                 res[index] = 0;
+//                 index++;
+//             }
+//         } else if (count.get(key) > 0) {
+//             while (key % 2 === 0 && count.has(key / 2)) {
+//                 key /= 2;
+//             }
+            
+//             while (count.has(key)) {
+//                 const freq = count.get(key);
+//                 if (freq > 0) {
+//                     if (freq > count.get(key * 2) ?? 0) {
+//                         return [];
+//                     }
+                
+//                     for (let i = 0; i < freq; i++) {
+//                         res[index] = key;   
+//                         index++;
+//                     }
+//                     count.set(key, 0);
+//                     count.set(key * 2, count.get(key * 2) - freq);
+//                 }
+                
+//                 key += key;
+//             }
+//         }
+//     }
+
+//     return res;
+// };
