f1102d631a0d1f001743002ce92820e75452ae06
Dolan <dolan_miu@hotmail.com>
2022-07-03 11:49:02 +0100

    Time: 121 ms (39.51%), Space: 42.9 MB (94.73%) - LeetHub

--- 136-single-number/136-single-number.js
+++ 136-single-number/136-single-number.js
@@ -2,6 +2,8 @@
  * @param {number[]} nums
  * @return {number}
  */
+// Time: O(n)
+// Space: O(1)
 var singleNumber = function(nums) {
     let output = 0;
     for (let i = 0; i < nums.length; i++) {
