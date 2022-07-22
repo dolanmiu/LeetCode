6538d066935062bab5440a837b7325048bbbc33e
Dolan <dolan_miu@hotmail.com>
2022-07-22 20:34:42 +0100

    Time: 300 ms (30.44%), Space: 61.4 MB (8.70%) - LeetHub

--- 497-random-point-in-non-overlapping-rectangles/497-random-point-in-non-overlapping-rectangles.js
+++ 497-random-point-in-non-overlapping-rectangles/497-random-point-in-non-overlapping-rectangles.js
@@ -8,97 +8,38 @@ var Solution = function(rects) {
         const [a, b, x, y] = r;
         return {
            rect: r,
-           area: Math.abs(x - a + 1) * Math.abs(y - b + 1),
+           area: (x - a + 1) * (y - b + 1),
         };
-    }).filter((r) => r.area > 0);
-    
-    this.sumArea = this.data.reduce((acc, curr) => acc + curr.area, 0);
-    console.log(this.data, this.sumArea)
-
+    });
 };
 
 /**
  * @return {number[]}
  */
 Solution.prototype.pick = function() {
-    let rand = Math.random() * this.sumArea;
-
-    const rect = weightedProcessing(this.data, rand);
+    // Time: O(2r)
+    // Space: O(1)
+    const randomRect = weightedRandom(this.data);
     
-    const [a, b, x, y] = rect;
+    const [a, b, x, y] = randomRect;
     
     const newX = randomIntFromInterval(a, x);
     const newY = randomIntFromInterval(b, y);
+    
     return [newX, newY];
-    // return [rect[0]+Math.floor(Math.random()*(rect[2]-rect[0]+1)), rect[1]+Math.floor(Math.random()*(rect[3]-rect[1]+1))]
-    
-    // Time: O(2r)
-    // Space: O(1)
-//     const randomRect = weightedRandom2(this.data);
-    
-//     const rect = randomRect;
-//     return [rect[0]+Math.floor(Math.random()*(rect[2]-rect[0]+1)), rect[1]+Math.floor(Math.random()*(rect[3]-rect[1]+1))];
-    
-    
-//     const [a, b, x, y] = randomRect;
-    
-//     const newX = randomIntFromInterval(a, x);
-//     const newY = randomIntFromInterval(b, y);
-    
-
-//     return [newX, newY];
 };
 
-function weightedRandom3(args) {
-    let area = Math.random() * this.sumArea;
-    let i = 0;
-    while(area - args[i].area > 0) {
-        area -= args[i].area;
-        i++;
-    }
-    
-    const rect = args[i].rect;
-    
-    return rect;
-}
-
-function weightedRandom2(args) {
-    let area = Math.random() * this.sumArea;
-    let i = 0;
-    while(area - args[i].area > 0) {
-        area -= args[i].area;
-        i++;
-    }
-    
-    const rect = args[i].rect;
-    
-    return rect;
-}
-
-function weightedProcessing(args, rand) {
-    // Time: O(r)
+function weightedRandom(args, randomVal) {
+    // Time: O(2r)
     // Space: O(1)
+    const totalArea = args.reduce((acc, curr) => acc + curr.area, 0);
+    const rand = Math.random() * totalArea;
     
     let sum = 0;
     
     for (const { rect, area } of args) {
         sum += area;
-        if (sum > rand) {
-            return rect;
-        }
-    }
-}
-
-function weightedRandom(args) {
-    // Time: O(r)
-    // Space: O(1)
-    const rand = Math.random() * this.sumArea;
-    
-    let sum = 0;
-    
-    for (const { rect, area } of args) {
-        sum += area;
-        if (sum > rand) {
+        if (sum >= rand) {
             return rect;
         }
     }
